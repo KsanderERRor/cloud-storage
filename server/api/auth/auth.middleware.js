41f8449ee4dfc686fd642a1facc95db22136a3eb
max <serokurovmaksim91@gmail.com>
2023-03-17 00:21:12 +0200

    tools for pretty code  ready  to use

--- server/api/auth/auth.middleware.js
+++ server/api/auth/auth.middleware.js
@@ -1,17 +1,18 @@
+/* eslint-disable no-undef */
 module.exports = {
+  checkUser: async (req, res, next) => {
+    try {
+      const user = await userService.findByEmail(req.body.email);
 
-    checkUser: async (req, res, next) => {
-        try {
-            const user = await userService.findByEmail(req.body.email)
+      if (!user) {
+        res
+          .status(400)
+          .json({ message: `User with  email ${user.email} not found` });
+      }
 
-
-            if (!user) {
-                res.status(400).json({ message: `User with  email ${user.email} not found` })
-            }
-
-            next()
-        } catch (e) {
-            throw new Error(e)
-        }
+      next();
+    } catch (e) {
+      throw new Error(e);
     }
-}
+  },
+};
--- server/api/user/user.router.js
+++ server/api/user/user.router.js
@@ -1,10 +1,13 @@
-const userRouter = require('express').Router()
+const userRouter = require("express").Router();
 
+const mdlw = require("./user.middleware");
+const userController = require("./user.controller");
 
-const mdlw = require('./user.middleware')
-const userController = require('./user.controller')
-
-
-userRouter.post('/registration', mdlw.userValidator, mdlw.checkUserDyplicates, userController.createUser)
+userRouter.post(
+  "/registration",
+  mdlw.userValidator,
+  mdlw.checkUserDyplicates,
+  userController.createUser,
+);
 
 module.exports = userRouter;
