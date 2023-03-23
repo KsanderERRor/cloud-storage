b7ea7f1cc203166ff87ec95f12bfeae9ca114ae7
max <serokurovmaksim91@gmail.com>
2023-03-23 10:39:19 +0200

    fix

--- server/configs/variables.js
+++ server/configs/variables.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 5200,
-    MONGO_URL: 'mongodb+srv://serokurovmaksim91:serokurovmaksim91serokurovmaksim91@cloud.znu5rpf.mongodb.net/?retryWrites=true&w=majority'
+    MONGO_URL: 'mongodb://mongo:27017'
 };
