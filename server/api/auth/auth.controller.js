0eb1a7a7644c45f403b92d7180797d4a8b6ee0d5
max <serokurovmaksim91@gmail.com>
2023-03-29 00:08:58 +0300

    create token

--- server/api/auth/auth.controller.js
+++ server/api/auth/auth.controller.js
@@ -1,8 +1,21 @@
 const { checkUser } = require("./auth.middleware");
-
+const oauthService = require('../../services/oauth.service')
+const service = require('./auth.service')
 module.exports = {
-  loginUser: async (req, res) => {
+  loginUser: async (req, res, next) => {
     try {
-    } catch (e) {}
+      const user = req.locals.user;
+
+      await oauthService.checkPasswords(user.password, req.body.password);
+      const tokenPair = oauthService.generateAccessTokenPair({...user});
+
+      await service.createOauthPair({ ...tokenPair, user: user._id });
+      res.json({
+        ...tokenPair,
+        user
+      });
+    } catch (e) {
+      next(e);
+    }
   },
 };
--- server/api/auth/auth.middleware.js
+++ server/api/auth/auth.middleware.js
@@ -1,15 +1,17 @@
-/* eslint-disable no-undef */
+const userService = require('../user/user.service')
 module.exports = {
   checkUser: async (req, res, next) => {
     try {
       const user = await userService.findByEmail(req.body.email);
-
+      // console.log(user,'user auth mdlw');
       if (!user) {
         res
           .status(400)
           .json({ message: `User with  email ${user.email} not found` });
       }
 
+      req.locals = {...req.locals, user };
+      // console.log(req.locals,"req---locals"); 
       next();
     } catch (e) {
       throw new Error(e);
