3d49538c235dae8c28cc7f9e4da97806bda8771c
max <serokurovmaksim91@gmail.com>
2023-04-13 00:20:43 +0300

    login and logout user

--- server/api/auth/auth.router.js
+++ server/api/auth/auth.router.js
@@ -1,9 +1,10 @@
 /* eslint-disable no-undef */
 authRouter = require('express').Router();
 
-const { loginUser } = require('./auth.controller');
+const controller = require('./auth.controller');
 const mdlw = require('./auth.middleware');
 
-authRouter.use('/', mdlw.checkUser, loginUser);
+authRouter.post('/login', mdlw.checkUser, controller.loginUser);
+authRouter.post('/logout', mdlw.validateAccessToken, controller.logoutUser);
 
 module.exports = authRouter;
