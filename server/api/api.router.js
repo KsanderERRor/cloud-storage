7035a6b4da3da5829c6879e7c44edcc56d32514c
max <serokurovmaksim91@gmail.com>
2023-04-25 21:17:54 +0300

    2

--- server/api/api.router.js
+++ server/api/api.router.js
@@ -2,8 +2,6 @@ const apiRouter = require('express').Router();
 
 const userRouter = require('./user/user.router');
 const authRouter = require('./auth/auth.router');
-
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
-
 module.exports = apiRouter;
