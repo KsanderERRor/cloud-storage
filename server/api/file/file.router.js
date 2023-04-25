5d27ad37c8c0e7a5c2a923ae135bac4ad0deb190
max <serokurovmaksim91@gmail.com>
2023-04-25 21:15:39 +0300

    '2' '2'

--- server/api/file/file.router.js
+++
@@ -1,8 +0,0 @@
-const fileRouter = require('express').Router();
-
-const oauthMdlw = require('../auth/auth.middleware');
-const fileController = require('./file.controller');
-
-fileRouter.post('/', oauthMdlw.validateAccessToken, fileController.createFile);
-
-module.exports = fileRouter;
--- server/api/file/file.service.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-unused-vars */
-const fs = require('node:fs/promises');
-const path = require('node:path');
-
-module.exports = {
-  createFile: (data) => data
-};
